ae290c1f2ceeed7fc1af93ddb28b8daf70ea5330
thygolem <85223163+thygolem@users.noreply.github.com>
2022-10-10 13:49:33 +0200

    Create changeColor.cs

---
+++ changeColor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Renderer Cube;
+
+    public Material MainMaterial;
+    Color color;
+
+
+    void Start()
+    {
+        Cube = GetComponent<Renderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        color = Cube.material.color;
+        Cube.material.color = Color.red;
+    }
+    private void OnMouseExit()
+    {
+        Cube.material.color = color;
+        // Cube.material = MainMaterial;
+    }
+    
+}
