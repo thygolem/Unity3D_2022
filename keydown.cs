1f5ea4b1e9d6f91a92a2a33eed6d26cac2ab35a5
thygolem <85223163+thygolem@users.noreply.github.com>
2022-10-10 12:37:33 +0200

    Create keydown.cs

---
+++ keydown.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keydowns : MonoBehaviour
+{
+    private int vertical;
+    private int horizontal;
+
+
+    public Text verticalUI;
+    public Text horizontalUI;
+
+    public Text selectedCubeText;
+
+
+    public GameObject CUBES; 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        teclasMovimientoSeleccion();
+        GetCubes();
+
+    }
+
+    void teclasMovimientoSeleccion()
+    {
+        if (Input.GetKeyDown("up"))
+            {
+                vertical = vertical +1;
+                verticalUI.text = vertical.ToString("");
+                print(verticalUI);
+            }
+        else if (Input.GetKeyDown("down"))
+            {
+                vertical = vertical -1;
+                verticalUI.text = vertical.ToString("");
+                print(verticalUI);
+            }
+
+
+        if (Input.GetKeyDown("right"))
+            {
+                horizontal = horizontal +1;
+                horizontalUI.text = horizontal.ToString("");
+                print(verticalUI);
+            }
+        else if (Input.GetKeyDown("left"))
+            {
+                horizontal = horizontal -1;
+                horizontalUI.text = horizontal.ToString("");
+                print(horizontalUI);
+            }
+    }
+
+
+    private void Awake()
+    {
+    }
+
+    private void GetCubes()
+    {
+        Transform child = transform.GetChild(transform.childCount - 1);
+        // Debug.Log("Child Count: " + transform.childCount);
+        // Debug.Log(child.name);
+    }
+
+
+
+}
