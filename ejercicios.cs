e710fd8e0bd39184bee00d8af6caed6395f5a1bc
thygolem <85223163+thygolem@users.noreply.github.com>
2022-10-11 12:18:37 +0200

    Update ejercicios.cs

--- ejercicios.cs
+++ ejercicios.cs
@@ -22,3 +22,60 @@
     
     
 
+//////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////////       SWITCH       ///////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
+private void Awake()
+    {
+        EjercicioSwitch();
+
+    }
+
+    private void EjercicioSwitch(){
+        print($"El dia es {DiaSemana(numero1)}");
+
+    }
+
+
+    private string DiaSemana(int num)
+    {
+        string resultado = "";
+        switch (num)
+        {
+        case 7:
+            resultado = "Domingo";
+            break;    
+        case 6:
+            resultado = "Sábado";
+            break;
+        case 5:
+            resultado = "Viernes";
+            break;
+        case 4:
+            resultado = "Jueves";
+            break;
+        case 3:
+            resultado = "Miércoles";
+            break;
+        case 2:
+            resultado = "Martes";
+            break;
+        case 1:
+            resultado = "Lunes";
+            break;
+        default:
+            resultado = "NUmero de dia no válido";
+            break;
+    }
+    return resultado;
+
+
+
+    }
