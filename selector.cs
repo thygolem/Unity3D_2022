3a1db30e2af53dbbafa1040eb9d5220ff2ac50c0
thygolem <85223163+thygolem@users.noreply.github.com>
2022-10-11 09:49:54 +0200

    Create selector.cs

---
+++ selector.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+         // Debug.Log("Mouse is down");
+         
+         RaycastHit hitInfo = new RaycastHit();
+         bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+         if (hit) 
+         {
+             Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+             if (hitInfo.transform.gameObject.tag == "Construction")
+             {
+                 // Debug.Log ("It's working!");
+             } else {
+                 // Debug.Log ("nopz");
+             }
+         } else {
+             Debug.Log("No hit");
+         }
+         // Debug.Log("Mouse is down");
+     } 
+ }
+}
